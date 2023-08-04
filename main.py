1cb8bf3cc49e48a894d20753b793b5e13591d2ec
Rolando Alvarado <alvaradorolando67@gmail.com>
2023-08-04 16:02:07 -0600

    Primera parte del proyecto integrador completa

---
+++ main.py
@@ -0,0 +1,4 @@
+#Codigo del laberinto realizado por Rolando Alvarado
+print("Ingrese el nombre del jugador")
+nombre=input()
+print("Hola" , nombre, "bienvenido al juego del laberinto a divertirse.")
